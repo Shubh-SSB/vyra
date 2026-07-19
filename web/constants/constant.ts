f02e3630173dab8450a4b19ac94963e22100ef31
Shubham Bhatnagar <shubham.bhatnagar234@gmail.com>
2026-07-20 01:34:51 +0530

    Update constant.ts

--- web/constants/constant.ts
+++ web/constants/constant.ts
@@ -1,4 +1,4 @@
 const BASE_URL: string =
-  process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/";
+    process.env.NEXT_PUBLIC_API_URL ?? "https://3947-2409-40d2-2067-d856-b8a9-75b1-db44-112f.ngrok-free.app/";
 
-export { BASE_URL };
+export { BASE_URL };
